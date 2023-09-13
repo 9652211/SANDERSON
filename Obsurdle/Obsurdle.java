135c9c194553be42425cdcdbf472cb5c33195028
9652211 <89046778+9652211@users.noreply.github.com>
2023-09-13 08:22:11 -0600

    Add files via upload

---
+++ Obsurdle/Obsurdle.java
@@ -0,0 +1,21 @@
+
+public class Obsurdle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" -- -- -- -- -- ");
+		System.out.println("| O| B| S| U| R|");
+		System.out.println(" -- -- -- -- -- ");
+		System.out.println("| D| L| E| O| B|");
+		System.out.println(" -- -- -- -- -- ");
+		System.out.println("| S| U| R| D| L|");
+		System.out.println(" -- -- -- -- -- ");
+		System.out.println("| E| O| B| S| U|");
+		System.out.println(" -- -- -- -- -- ");
+		System.out.println("| R| D| L| E| O|");
+		System.out.println(" -- -- -- -- -- ");
+		System.out.println("| B| S| U| R| D|");
+		System.out.println(" -- -- -- -- -- ");
+	}
+
+}
