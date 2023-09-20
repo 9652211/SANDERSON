5f45fd40095023804299e7954ae76248245b0a89
9652211 <89046778+9652211@users.noreply.github.com>
2023-09-20 16:35:09 -0600

    Add files via upload

--- Obsurdle/Match.java
+++ Obsurdle/Match.java
@@ -2,15 +2,15 @@ public class Match {
 	private static String w = "";
 	private static String g = "";
 	public void setWords(String word, String guess) {
-		this.w = word;
-		this.g = guess;
+		this.w = word.toUpperCase();
+		this.g = guess.toUpperCase();
 	}
 	public static String matchText() {
 		String output = "|";
-		for(int j = 0; j<7; j++) {
+		for(int i = 0; i<7; i++) {
 			String check = " -";
-			for(int i = 0; i<7; i++) {
-				if(w.charAt(i)==g.charAt(j)) {
+			for(int j = 0; j<7; j++) {
+				if(g.charAt(i)==w.charAt(j)) {
 					if (i==j) {
 						check = " ■";
 					} else if (check != " ■") {
